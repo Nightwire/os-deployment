c21a8edd4c9b0695b5c5d951e8135cfd97215800
Robin Beismann <robin@beismann.biz>
2018-01-06 04:40:57 +0100

    Update osdeployment.ps1

--- osdeployment.ps1
+++ osdeployment.ps1
@@ -13,3 +13,10 @@ switch($PSVersionTable.Platform){
 
 #Ubuntu 16.04: Ubuntu16.04.3LTS
 Write-Host("Executing Nightwire OS Deployment on $localOS")
+
+if($localOS -eq "Ubuntu16.04.3LTS"){
+    $url = "https://raw.githubusercontent.com/Nightwire/os-deployment/master/motd.sh"
+    $output = "/etc/profile.d/motd.sh"
+
+    Invoke-WebRequest -Uri $url -OutFile $output
+}
